e46504f6d1604b44e30e41f53369a20e45e10b03
FrancescoAceto <acetof02@gmail.com>
2023-04-12 18:30:03 +0200

    Navbar Completa

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,22 @@
 <script>
+import NavbarItem from "./NavbarItem.vue"
  export default {
     name: 'Header',
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+        NavbarItem
+    }
  }
 </script>
 
 <template>
+    <NavbarItem>
 
+    </NavbarItem>
 </template>
 
 <style scoped>
---
+++ src/components/NavbarItem.vue
@@ -0,0 +1,81 @@
+<script>
+export default {
+    name: 'NavbarItem',
+    data() {
+        return {
+            NavbarLinks: [
+                {
+                    text: 'HOME',
+                },
+                {
+                    text: 'LANDING',
+                },
+                {
+                    text: 'PAGES',
+                },
+                {
+                    text: 'DOCS',
+                },
+                
+            ],
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="Navbar">
+        <div class="img-container">
+            <img src="../../public/img/logo-dark.png" alt="">
+        </div>
+        
+        <div class="Links" v-for="NavbarLinks in NavbarLinks">
+            <div>{{ NavbarLinks.text }} <i class="fa-solid fa-chevron-down"></i></div>
+        </div>
+
+        <div class="Icons-container">
+            <i class="fa-solid fa-magnifying-glass glass"></i>
+            <i class="fa-brands fa-github Small-icons"></i>
+            <i class="fa-solid fa-dolly Small-icons"></i>
+            <i class="fa-regular fa-user Small-icons"></i>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+    .Navbar{
+        display: flex;
+        justify-content: space-evenly;
+        padding: 20px;
+    }
+
+    .Navbar img{
+        width: 160px;
+    }
+
+    .Links:hover{
+        color: #2f55d4;
+        cursor: pointer;
+    }
+
+    .Icons-container{
+        display: flex;
+        gap: 10px;
+    }
+
+    .Small-icons{
+        background-color: #eaeefb;
+        color: #2f55d4;
+        padding: 10px;
+        border-radius: 5px;
+        box-shadow: 5px 4px 10px gray;
+    }
+
+    .glass{
+        padding: 10px;
+        color: gray;
+    }
+
+
+</style>
